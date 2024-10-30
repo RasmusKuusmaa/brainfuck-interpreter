f5b5ff171a138c7c4c55929dc8224e647dbff0bc
77f315 <77f315@gmail.com>
2024-10-30 19:45:50 +0200

    loops

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ def bf_interpreter(code):
     pointer = 0
     output = ""
 
+    loop = {}
+    stack = []
+    for idx, cmd in enumerate(code):
+        if cmd == "[":
+            stack.append(idx)
+        elif cmd == "]":
+            start = stack.pop()
+            loop[start] = idx
+            loop[idx] = start
+    
     code_pointer = 0
     while code_pointer < len(code):
         cmd = code[code_pointer]
@@ -14,13 +24,18 @@ def bf_interpreter(code):
         elif cmd == "+":
             memory[pointer] = (memory[pointer] + 1) % 256
         elif cmd == "-":
-            memory[pointer] = (memory[pointer] + 1) % 256
+            memory[pointer] = (memory[pointer] - 1) % 256
         elif cmd == ".":
             output += chr(memory[pointer])
-        
+        elif cmd == "[":
+            if memory[pointer] == 0:
+                code_pointer = loop[code_pointer]
+        elif cmd == "]":
+            if memory[pointer] != 0:
+                code_pointer = loop[code_pointer]
         code_pointer += 1
     return output
 
 print(bf_interpreter(
-    '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++.<-.>+.'
+    '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
 ))
